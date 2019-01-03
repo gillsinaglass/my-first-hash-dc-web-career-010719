38e2f51ae68e0b6b961675a1a6d02d1da4708a78
Ryan Gill <rdgill95@gmail.com>
2019-01-03 21:28:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,11 +29,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-  shipping_manifest["muskets"] => 2
+  shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-  shipping_mannifest["gun powder"] => 4
+  shipping_mannifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
   return shipping_manifest
